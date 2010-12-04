ea7e1fd6b6e0bd6092ac55cea845186f4abae095
iven <ivenvd@gmail.com>
2010-12-04 09:08:30 +0800

    Added event.{c,h}, a simple semaphore wrapper.

---
+++ src/libchatsys/event.h
@@ -0,0 +1,31 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  event.h
+ *
+ *    Description:  Interface of event.
+ *
+ *        Version:  1.0
+ *        Created:  2010年12月04日 08时15分37秒
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ *         Author:  Xu Lijian (ivenvd), ivenvd@gmail.com
+ *        Company:  USTC, China
+ *
+ * =====================================================================================
+ */
+
+#ifndef  EVENT_INC
+#define  EVENT_INC
+
+#include	<semaphore.h> 
+
+typedef sem_t Event;
+
+int event_init(Event *event, int value);
+int event_exit(Event *event);
+int event_post(Event *event);
+int event_wait(Event *event);
+
+#endif   /* ----- #ifndef EVENT_INC  ----- */
